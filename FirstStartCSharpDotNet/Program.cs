04e391ebdc95fb568db5eed1cf26cc44eef6dafa
kotlj <crotlj6@gmail.com>
2023-06-09 19:35:45 +0300

    done

---
+++ FirstStartCSharpDotNet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstStartCSharpDotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number <= 100)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("Fizz - Buzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+}
